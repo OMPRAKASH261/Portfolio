cc2b32bcbbc6a2d01b3f10af5227d3cbc7686355
OM PRAKASH <og04914@gmail.com>
2026-08-10 23:17:16 +0530

    Hero->Bg & GreenDot_ Animation

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,3 +1,32 @@
 export const Hero = () => {
-    return <section></section>;
+  return (
+    <section className="realtive min-h-screen flex items-center overflow-hidden">
+      {/* BG */}
+      <div className="absolute inset-0">
+        <img
+          src="/hero-bg.jpg"
+          alt="Hero image"
+          className="w-full h-full object-cover opacity-40"
+        />
+        <div className="absolute inset-0 bg-gradient-to-b from-background/20 via-background/80 to-background"></div>
+      </div>
+
+      {/* Green Dots */}
+      <div className="absolute inset-0 overflow-hidden pointer-events-none">
+        {[...Array(30)].map((_, i) => (
+            <div 
+                className="absolute w-1.5 h-1.5 rounded-full opactity-60"
+                style={{
+                    background: "#20B246",
+                    left: `${Math.random() * 100}%`,
+                    top: `${Math.random() * 100}%`,
+                    animation: `slow-drift ${15 + Math.random() * 20}s ease-in-out infinite`,
+                    animationDelay: `${Math.random() * 5}s`,
+                }}
+            />
+        ))}
+
+      </div>
+    </section>
+  );
 };
